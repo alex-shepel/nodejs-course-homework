b8244a5dea509392923f7a7f20e5c4375609cb0b
o.shepel <o.shepel@abtollc.com>
2022-06-14 16:20:40 +0300

    add "database connection successful" message

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
+  .then(() => console.log('Database connection successful'))
   .then(() => {
     app.listen(PORT, () => console.log('Server running on port 3000'));
   })
