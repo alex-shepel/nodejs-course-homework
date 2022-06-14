9b3bbc391f741cf0fcfdaf2becb09524bdeadfc9
o.shepel <o.shepel@abtollc.com>
2022-06-14 16:23:03 +0300

    clarify DB contact schema

--- models/contact.js
+++ models/contact.js
@@ -1,11 +1,28 @@
 const { Schema, model } = require('mongoose');
 
-const contactSchema = Schema({
-  name: String,
-  email: String,
-  phone: String,
-  favorite: Boolean,
-});
+const contactSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Set name for contact'],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+      match: /^\(\d{3}\) \d{3}-\d{4}$/,
+    },
+    favorite: {
+      type: String,
+      default: false,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  },
+);
 
 const Contact = model('contact', contactSchema);
 
