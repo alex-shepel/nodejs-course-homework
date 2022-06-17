59359435f2b825c76c6d7657dd3909f882772ba9
o.shepel <o.shepel@abtollc.com>
2022-06-17 22:53:06 +0300

    add password hashing on registration

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -1,11 +1,17 @@
+const bcrypt = require('bcryptjs');
 const { User } = require('../../models/user');
 const { createError } = require('../../helpers');
 
+const SALT_LENGTH = 10;
+
 const register = async (req, res) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
+
   const existingUser = await User.findOne({ email });
   if (existingUser) throw createError(409, 'Email in use');
-  const user = await User.create(req.body);
+
+  const hashPassword = await bcrypt.hash(password, SALT_LENGTH);
+  const user = await User.create({ email, password: hashPassword });
   res.status(201).json({
     user: {
       email: user.email,
