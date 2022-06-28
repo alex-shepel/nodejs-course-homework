91a5a57cad7a750aff17af5e0fe07df7f47783c2
o.shepel <o.shepel@abtollc.com>
2022-06-28 18:07:11 +0300

    implement api/contacts route pagination

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -2,10 +2,13 @@ const { Contact } = require('../../models/contact');
 
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
-  const contacts = await Contact.find(
-    { owner },
-    '-createdAt -updatedAt',
-  ).populate('owner', 'email');
+  const page = Number(req.query.page);
+  const limit = Number(req.query.limit);
+  const skip = (page - 1) * limit;
+  const contacts = await Contact.find({ owner }, '-createdAt -updatedAt', {
+    skip,
+    limit,
+  }).populate('owner', 'email');
   res.json(contacts);
 };
 
